5a2ac64afe317ec4eddfa80b3c0b5b935f136cd7
JuampiJPT <juanpedrotagliaferro@gmail.com>
2025-02-22 11:40:08 -0300

    preline instalation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import PrelineScript from "./components/PrelineScript";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        
       </body>
+      <PrelineScript />
     </html>
   );
 }
