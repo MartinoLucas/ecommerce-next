6b752b0dd88fdb397517436819f052c4e2f72afe
Lucas Martino <lucasmartino241@gmail.com>
2025-04-03 18:56:07 -0300

    feat: social media svgs

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -11,7 +11,8 @@ const Footer = () => {
         mx-auto
         max-w-[66rem]
         rounded-t-[26px]        /* Solo redondea la parte superior */
-        
+        lg:block
+        hidden
         shadow-high-top
         p-4
 
@@ -25,7 +26,7 @@ const Footer = () => {
         </span>
 
         {/* Enlaces del Footer */}
-        <div className="flex flex-wrap items-center gap-4">
+        <div className="flex flex-wrap items-center gap-6">
           {
             BRAND_SOCIAL_MEDIA.map((socialMedia, index) => (
               <a
@@ -33,38 +34,18 @@ const Footer = () => {
                 href={socialMedia.url}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-foreground hover:text-primary-dark focus:text-primary-dark text-sm"
+                className="text-foreground hover:text-primary-dark focus:text-primary-dark text-sm flex flex-row items-center gap-2"
               >
-                {/* <img
+                <img
                   src={socialMedia.icon}
                   alt={socialMedia.name}
                   className="w-6 h-6"
-                /> */}
+                />
                 {socialMedia.name}
               </a>
             ))
           }
         </div>
-        {/* <div className="flex flex-wrap items-center gap-4">
-          <a
-            href="#"
-            className="text-foreground hover:text-primary-dark focus:text-primary-dark text-sm"
-          >
-            Privacy
-          </a>
-          <a
-            href="#"
-            className="text-foreground hover:text-primary-dark focus:text-primary-dark text-sm"
-          >
-            Terms
-          </a>
-          <a
-            href="#"
-            className="text-foreground hover:text-primary-dark focus:text-primary-dark text-sm"
-          >
-            Contact
-          </a>
-        </div> */}
       </div>
     </footer>
   );
