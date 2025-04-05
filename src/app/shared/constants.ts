b39279e42b81de737ea7fef8b35efe92627d5bd3
kochq <matukoch@live.com>
2025-04-05 14:19:55 -0300

    fix: Iconos

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -9,17 +9,17 @@ export const BRAND_BENEFITS = [
     {
         title: "Selección Boutique",
         description: "Vinos únicos, de bodegas pequeñas, con historia.",
-        icon: "icons/vinos_boutique.svg",
+        icon: "/icons/vinos_boutique.svg",
     },
     {
         title: "Curados por expertos",
         description: "Cada botella está elegida con criterio y pasión.",
-        icon: "icons/expertos.svg",
+        icon: "/icons/expertos.svg",
     },
     {
         title: "Experiencia única",
         description: "Sorprendé tu paladar con ediciones limitadas y raras.",
-        icon: "icons/experiencia_unica.svg",
+        icon: "/icons/experiencia_unica.svg",
     }
 ];
 
@@ -27,12 +27,12 @@ export const BRAND_SOCIAL_MEDIA = [
     {
         name: "WhatsApp",
         url: "https://www.wa.me/2364360402",
-        icon: "icons/whatsapp.svg",
+        icon: "/icons/whatsapp.svg",
     },
     {
         name: "Instagram",
         url: "https://www.instagram.com/cuartageneracionvinoss",
-        icon: "icons/instagram.svg",
+        icon: "/icons/instagram.svg",
     },
 ];
 
@@ -42,44 +42,44 @@ export const ABOUT_US = {
 };
 
 export const LOCATION_INFO = {
-title: "¿Dónde estamos?",
-description: "Encontranos en nuestra vinoteca boutique en Junín, Buenos Aires.",
-mapEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d262.17246024941966!2d-60.94145149915558!3d-34.595738687613576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b8eb50ad0d7ff7%3A0xca0956eefa073b6b!2sCuarta%20Generaci%C3%B3n%20Vinos%20Boutique!5e0!3m2!1ses!2sar!4v1743715487202!5m2!1ses!2sar"
+    title: "¿Dónde estamos?",
+    description: "Encontranos en nuestra vinoteca boutique en Junín, Buenos Aires.",
+    mapEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d262.17246024941966!2d-60.94145149915558!3d-34.595738687613576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b8eb50ad0d7ff7%3A0xca0956eefa073b6b!2sCuarta%20Generaci%C3%B3n%20Vinos%20Boutique!5e0!3m2!1ses!2sar!4v1743715487202!5m2!1ses!2sar"
 };
 
 export const FEATURED_WINES_TEXTS = {
-title: "Vinos Recomendados",
+    title: "Vinos Recomendados",
 };
 
 export const FOUR_CLUB = {
-title: `Unite al ${BRAND_NAME_REDUCED} Club 🍷`,
-description: `Recibí cada mes una caja sorpresa con vinos seleccionados, notas de cata, stickers y mucho más. Ideal para descubrir nuevas etiquetas y ampliar tu paladar.`,
-cta: "Pronto disponible",
+    title: `Unite al ${BRAND_NAME_REDUCED} Club 🍷`,
+    description: `Recibí cada mes una caja sorpresa con vinos seleccionados, notas de cata, stickers y mucho más. Ideal para descubrir nuevas etiquetas y ampliar tu paladar.`,
+    cta: "Pronto disponible",
 };
 
 export const BENEFITS_TEXT = {
-title: "¿Por qué elegirnos?",
+    title: "¿Por qué elegirnos?",
 };
 
 export const BRAND_NAV_OPTIONS = [
     {
         label: "Sobre Nosotros",
         href: "#",
-        icon: "icons/sobre_nosotros.svg",
+        icon: "/icons/sobre_nosotros.svg",
     },
     {
         label: "Donde Estamos",
         href: "#",
-        icon: "icons/sobre_nosotros.svg",
+        icon: "/icons/sobre_nosotros.svg",
     },
     {
         label: "Porque Elegirnos",
         href: "#",
-        icon: "icons/sobre_nosotros.svg",
+        icon: "/icons/sobre_nosotros.svg",
     },
     {
         label: "Blog",
         href: "#",
-        icon: "icons/sobre_nosotros.svg",
+        icon: "/icons/sobre_nosotros.svg",
     },
 ];
